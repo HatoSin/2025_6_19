bf6411a3dc4e2239624f9069baf8e42f9ba05a16
kuwata23 <TTC2550SG0041@edu.tech.ac.jp>
2025-06-19 10:59:35 +0900

    Unityファイルの追加
    unity上でキャンディを消す処理の追加

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    //場外に出たオブジェクトを消す処理
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Candy")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
